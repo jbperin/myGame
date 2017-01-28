0917af5d2c11c401e798854e64802971a794df5d
jbperin <jbperin@users.noreply.github.com>
2017-01-28 18:24:45 +0100

    make gl renderer as a Fragment rather than an activity

--- app/src/main/java/com/jibepe/activities/MainActivity.java
+++ app/src/main/java/com/jibepe/activities/MainActivity.java
@@ -1,139 +1,42 @@
 package com.jibepe.activities;
 
 
-
-import android.app.ActivityManager;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ConfigurationInfo;
-import android.opengl.GLSurfaceView;
-import android.os.Bundle;
-import android.os.Environment;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.RelativeLayout;
-import com.jibepe.labo3d.*;
-import com.jibepe.model.SceneGraph;
-import com.jibepe.objparser.ObjLoader;
-import com.jibepe.render3d.GLES20Renderer;
-import com.jibepe.util.DownloadFilesTask;
-import com.jibepe.util.DownloadHelper;
-
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-
+import com.jibepe.labo3d.R;
 
 public class MainActivity extends AppCompatActivity {
-	
-	private final String TAG = "MainActivity";
 
-    private GLES20View theView;
 
-    GLES20Renderer mRenderer = null;
-    SceneGraph theSceneGraph = null;
 
-    /*********************************************/
-    /***                PROCESS                 **/
-    /*********************************************/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    	
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        //DownloadFiles();
-        TextureHandler.getInstance().reset();
-        TextureHandler.getInstance().setContext(getApplication());
-        ShaderHandler.getInstance().reset();
-        ShaderHandler.getInstance().setContext(getApplication());
+//        // Begin the transaction
+//        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+//        // Replace the contents of the container with the new fragment
+//        ft.replace(R.id.render3d_placeholder, (Fragment) new glRenderFragment());
+//        // or ft.add(R.id.your_placeholder, new FooFragment());
+//        // Complete the changes added above
+//        ft.commit();
 
-        theSceneGraph = new SceneGraph();
-        //File file = new File(Environment.getExternalStoragePublicDirectory(
-        //        Environment.DIRECTORY_PICTURES), "scene.mtl");
-        ObjLoader mSceneLoader = new ObjLoader(getApplication());
-        mSceneLoader.loadModel("scene");
-        theSceneGraph.addObj("scene", mSceneLoader);
-
-        ObjLoader mPersoLoader = new ObjLoader(getApplication());
-        mPersoLoader.loadModel("plantexture");
-        theSceneGraph.addObj("plantexture", mPersoLoader);
-
-        //try {
-            //DownloadHelper.DownloadFile("scene.obj");
-            //DownloadHelper.DownloadFile("scene.obj");
-        //} catch (IOException e) {
-        //    e.printStackTrace();
-        //}
-        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
-        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
-
-        if (supportsEs2)
-		{
-            mRenderer = new GLES20Renderer();
-            // Request an OpenGL ES 2.0 compatible context.
-            //setEGLContextClientVersion(2);
-
-            // Create an OpenGL ES 2.0 context.
-			theView = new GLES20View(getApplication(),mRenderer, theSceneGraph);
-            theView.setEGLContextClientVersion(2);
-            // Set the Renderer for drawing on the GLSurfaceView
-            theView.setRenderer(mRenderer);
-            theView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
-
-			RelativeLayout outerView = 
-	                (RelativeLayout)this.findViewById(R.id.lelayout);
-	        View view1 =  this.findViewById(R.id.txt2glsurf);
-	        outerView.removeView(view1);
-	        outerView.addView(theView,0);
-		}
 
     }
 
-    private void DownloadFiles() {
-        String DownloadUrl = "http://jb.perin.pagesperso-orange.fr/";
-        String fileName = "scene.mtl";
 
-        boolean canW = DownloadHelper.isExternalStorageWritable();
-        boolean canR = DownloadHelper.isExternalStorageReadable();
-        if(canW == canR == true) {
 
-            File folder = getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS);
-            //DownloadHelper.verifyStoragePermissions(this);
-            String FullUrl = DownloadUrl;
-            URL url = null;
-            try {
-                url = new URL(FullUrl);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
-            if (url != null) {
-                new DownloadFilesTask(url, folder).execute(fileName);
-            }
-        }
-    }
 
-    @Override
-    protected void onStart() {
-    	super.onStart();
-    	
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-    
-    @Override
-    protected void onDestroy() {
-      // Unregister since the activity is about to be closed.
-      super.onDestroy();
-    }    
-  
-    
     /*********************************************/
     /***                GUI                     **/
     /*********************************************/
@@ -156,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
 
 
 
---
+++ app/src/main/java/com/jibepe/activities/glRenderFragment.java
@@ -0,0 +1,186 @@
+package com.jibepe.activities;
+
+
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.support.v4.app.Fragment;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.FragmentActivity;
+import android.view.*;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import com.jibepe.labo3d.*;
+import com.jibepe.model.SceneGraph;
+import com.jibepe.objparser.ObjLoader;
+import com.jibepe.render3d.GLES20Renderer;
+import com.jibepe.util.DownloadFilesTask;
+import com.jibepe.util.DownloadHelper;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class glRenderFragment extends Fragment {
+	
+	private final String TAG = "glRenderFragment";
+
+    private GLES20View theView;
+
+    GLES20Renderer mRenderer = null;
+    SceneGraph theSceneGraph = null;
+    FragmentActivity listener;
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.fragment_render3d);
+//    }
+
+
+    // This event fires 1st, before creation of fragment or any views
+    // The onAttach method is called when the Fragment instance is associated with an Activity.
+    // This does not mean the Activity is fully initialized.
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    // This event fires 2nd, before views are created for the fragment
+    // The onCreate method is called when the Fragment instance is being created, or re-created.
+    // Use onCreate for any standard setup that does not require the activity to be fully created
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    // This event is triggered soon after onCreateView().
+    // onViewCreated() is only called if the view returned from onCreateView() is non-null.
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+    }
+    // This method is called when the fragment is no longer connected to the Activity
+    // Any references saved in onAttach should be nulled out here to prevent memory leaks.
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+    // This method is called after the parent Activity's onCreate() method has completed.
+    // Accessing the view hierarchy of the parent activity must be done in the onActivityCreated.
+    // At this point, it is safe to search for activity View objects by their ID, for example.
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    /*********************************************/
+    /***                PROCESS                 **/
+    /*********************************************/
+    @Override
+    public View  onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        //View renderView = inflater.inflate(R.layout.fragment_render3d, container, false);
+
+        FragmentActivity faActivity  = (FragmentActivity)    super.getActivity();
+
+        LinearLayout llLayout    = (LinearLayout)    inflater.inflate(R.layout.fragment_render3d, container, false);
+        //DownloadFiles();
+        TextureHandler.getInstance().reset();
+        TextureHandler.getInstance().setContext(faActivity.getApplication());
+        ShaderHandler.getInstance().reset();
+        ShaderHandler.getInstance().setContext(faActivity.getApplication());
+
+        theSceneGraph = new SceneGraph();
+        //File file = new File(Environment.getExternalStoragePublicDirectory(
+        //        Environment.DIRECTORY_PICTURES), "scene.mtl");
+        ObjLoader mSceneLoader = new ObjLoader(faActivity.getApplication());
+        mSceneLoader.loadModel("scene");
+        theSceneGraph.addObj("scene", mSceneLoader);
+
+        ObjLoader mPersoLoader = new ObjLoader(faActivity.getApplication());
+        mPersoLoader.loadModel("plantexture");
+        theSceneGraph.addObj("plantexture", mPersoLoader);
+
+        //try {
+            //DownloadHelper.DownloadFile("scene.obj");
+            //DownloadHelper.DownloadFile("scene.obj");
+        //} catch (IOException e) {
+        //    e.printStackTrace();
+        //}
+        final ActivityManager activityManager = (ActivityManager) faActivity.getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+
+        if (supportsEs2)
+		{
+            mRenderer = new GLES20Renderer();
+            // Request an OpenGL ES 2.0 compatible context.
+            //setEGLContextClientVersion(2);
+
+            // Create an OpenGL ES 2.0 context.
+			theView = new GLES20View(faActivity.getApplication(),mRenderer, theSceneGraph);
+            theView.setEGLContextClientVersion(2);
+            // Set the Renderer for drawing on the GLSurfaceView
+            theView.setRenderer(mRenderer);
+            theView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+
+			LinearLayout outerView =
+	                (LinearLayout)faActivity.findViewById(R.id.lelayout);
+	        View view1 =  faActivity.findViewById(R.id.txt2glsurf);
+            llLayout.removeView(view1);
+            llLayout.addView(theView,0);
+		}
+        return (llLayout);
+    }
+
+    private void DownloadFiles() {
+        String DownloadUrl = "http://jb.perin.pagesperso-orange.fr/";
+        String fileName = "scene.mtl";
+
+        boolean canW = DownloadHelper.isExternalStorageWritable();
+        boolean canR = DownloadHelper.isExternalStorageReadable();
+        if(canW == canR == true) {
+
+            File folder = super.getActivity().getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS);
+            //DownloadHelper.verifyStoragePermissions(this);
+            String FullUrl = DownloadUrl;
+            URL url = null;
+            try {
+                url = new URL(FullUrl);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            if (url != null) {
+                new DownloadFilesTask(url, folder).execute(fileName);
+            }
+        }
+    }
+
+    @Override
+    public void onStart() {
+    	super.onStart();
+    	
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+    
+    @Override
+    public void onDestroy() {
+      // Unregister since the activity is about to be closed.
+      super.onDestroy();
+    }    
+  
+
+
+}
